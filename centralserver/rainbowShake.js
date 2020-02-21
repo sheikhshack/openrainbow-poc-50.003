cf9d2d717d21f3e20a3c6035bbc81c15c72fe18b
Unknown <redfreak97@gmail.com>
2020-02-21 16:53:58 +0800

    // added Express for REST API

--- centralserver/rainbowShake.js
+++ centralserver/rainbowShake.js
@@ -8,8 +8,6 @@ const rainbowSDK = new NodeSDK(adminCredentials);
 rainbowSDK.start();
 
 
-
-
 function init() {
 
     rainbowSDK.events.on('rainbow_onerror', function(err) {
@@ -18,28 +16,48 @@ function init() {
     });
 }
 
-function createGuests(talkTime) {
-        rainbowSDK.admin.createAnonymousGuestUser(talkTime).then((guest) => {
+async function createGuests(talkTime) {
+        // I simplified the process with async-await because i lazy type
+        let result = await rainbowSDK.admin.createAnonymousGuestUser(talkTime);
 
-            console.log("Guest has been created with login: " + guest.loginEmail + "and password : " + guest.password);
-            return {
-                "loginID": guest.loginEmail,
-                "loginPass": guest.password
-            };
+        // emulates sending of first text
+        // await rainbowSDK.im.sendMessageToJid("Hey there. I am CSA Sheikh here", result.jid);
+        // due to shit APIS, forced to create bubble instead
+        // rainbowSDK.bubbles.createBubble("Test", "A testing mechanism", false).then(function(bubble) {
+        //     rainbowSDK.bubbles.inviteContactToBubble(result, bubble, false, false, "non");
+        //
+        // }).catch((err) =>
+        // {
+        //
+        // });
 
-        }).catch((err) => {
-            console.log("OnError: ", err);
-            return err;
+        return {
+            "loginID" : result.loginEmail,
+            "loginPass" : result.password
+        }
 
-        });
+
+        // rainbowSDK.admin.createAnonymousGuestUser(talkTime).then((guest) => {
+        //
+        //     console.log("Guest has been created with login: " + guest.loginEmail + "and password : " + guest.password);
+        //     return {
+        //         "loginID": guest.loginEmail,
+        //         "loginPass": guest.password
+        //     };
+        //
+        // }).catch((err) => {
+        //     console.log("OnError: ", err);
+        //     return err;
+        //
+        // });
 
 
 }
 
 async function queryAgentStatus(agentEmail) {
-       let data = await rainbowSDK.contacts.getContactByLoginEmail(agentEmail);
-       console.log(data.presence);
-       return data.presence;
+       //let data = await rainbowSDK.contacts.getContactByLoginEmail(agentEmail);
+       let data2 = await rainbowSDK.contacts.getRosters();
+       return data2
 
         // rainbowSDK.contacts.getContactByLoginEmail(agentEmail).then((queriedUser) => {
         //     if (queriedUser) {
