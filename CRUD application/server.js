76a173c71da8e4ef83b3533284c6a1fdf812a61e
tinkitwong <tinkitwong@gmail.com>
2020-03-09 20:58:24 +0800

    Modifed MongoDB

--- CRUD application/server.js
+++ CRUD application/server.js
@@ -6,33 +6,51 @@ const app = express()
 var mongoose = require("mongoose")
 
 const MongoClient = require('mongodb').MongoClient;
-const assert = require('assert');
+const assert = require('assert')
 
 // Connection URL
-const url = 'mongodb://localhost';
+const url = 'mongodb://localhost'
+// DataBase Name
+const dbName = "sutdproject"
 
+// Create a new MongoClient
+const client = new MongoClient(url,  {useUnifiedTopology: true})
 
 // Use connect method to connect to the Server
-MongoClient.connect(url,  {useUnifiedTopology: true }, function(err, client) {
-                    assert.equal(null, err);
-                    const db = client.db("sutdproject");
+client.connect(function(err, client) {
+                    assert.equal(null, err)
+                    console.log("Connected correctly to server")
                     
-                    const cursor = db.collection('Department').find({
-                      'agentList':{ $elemMatch:{availability : true}}
-                    });
-
-                    function iterateFunc(doc) {
-                       console.log(JSON.stringify(doc, null, 4));
-                    }
-
-                    function errorFunc(error) {
-                       console.log(error);
-                    }
-
-                    cursor.forEach(iterateFunc, errorFunc);
+                    const db = client.db(dbName)
                     
+             
+                    checkAvail(db, function() {
+                                  })
+               
+               
                     
-});
+})
+
+function checkAvail(db, callback) {
+                  // Get the Departments collection
+                  const collection = db.collection('Agents');
+                  // Perform the find function
+                  collection.find({
+                    'availability': true
+                  }).toArray(function(err, docs) {
+                  assert.equal(err, null);
+                  console.log("Found the following records");
+                  console.log(docs)
+                  return callback(docs);
+             });
+}
+
+const updateavail = function(db, callback) {
+                  // Get the documents collection
+                  const collection = db.collection('Agents');
+                  // Update document where a is 2, set b equal to 1
+                  collection.insert
+}
 
 
 
