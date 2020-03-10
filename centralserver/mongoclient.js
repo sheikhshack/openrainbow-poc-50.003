35a8a2e6d5d9b7044d913ce0eaaf801bfa438ce4
tinkitwong <tinkitwong@gmail.com>
2020-03-10 18:35:42 +0800

    Updated MongoDB - Added Helper Functions

--- centralserver/mongoclient.js
+++ centralserver/mongoclient.js
@@ -1,3 +1,4 @@
+
 // Setup Express web application
 const bodyParser = require("body-parser");
 
@@ -17,13 +18,24 @@ client.connect( function(err, client) {
                     assert.equal(null, err);
                     console.log("Connected correctly to server");
                     const db = client.db(dbName);
+
+                    //toggleAvail("3006bf0f41a74dedb0c8e4da79b10be8@sandbox-all-in-one-rbx-prod-1.rainbow.sbg")
+              
+               
+                    
+               
+               
+
                     // checkAvail("Graduate Office","Chat")
+
 });
 
-
-// console log works to push json
-// return function cant be called in the connect block :Pending Promise
-async function checkAvail(departmentID, communication) {
+/*
+Checks availability of ALL agents of a specific department
+------------------------------------
+checkAvail("Graduate Office","Chat")
+ */
+ async function checkAvail(departmentID, communication) {
                   // Get the Departments collection
                   let result = await client.db(dbName).collection('Agent').find({
                       'availability': true,
@@ -35,21 +47,129 @@ async function checkAvail(departmentID, communication) {
 
              }
 
+/*
+ Adds new Agent as a new document in collection "Agent"
+ Other fields will be given a default value
+ ------------------------------------------------------------------------------
+ addAgent("AAF","testjid", "tinkitishere", ["Chat", "Audio"], "Tin Kit Office")
+ */
+async function addAgent(_id, jid, name, typeOfComm, departmentID){
+    await client.db(dbName).collection('Agent').insertOne({
+                                                    '_id' : _id,
+                                                    'jid' : jid,
+                                                    'Department_id' : departmentID,
+                                                    'name' : name,
+                                                    'availability' : true,
+                                                    'typeOfComm' : typeOfComm,
+                                                    'currentActiveSessions' : 0,
+                                                    'resrve' : 0
+                                                    }, function(err, res) {
+                                                    if (err) throw err;
+                                                    console.log("Document inserted")
+                                                    })
+}
+
+/*
+ Modify 1 CSA Agent propertie(s) identifiable by JID
+ let newProperties = {'currentActiveSessions' : 1 , 'availability' : false}
+ newProperties is a JSON object
+ ---------------------------------------------------------
+ modifyCommAndDept(['Audio', 'Chat'], '001', newProperties)
+ */
+async function modifyCommAndDept(typeOfComm, Department_id, newProperties) {
+    let oldProperties = {'typeOfComm' : typeOfComm , 'Department_id' : Department_id}
+    await client.db(dbName).collection('Agent').updateOne(
+                                                          oldProperties,
+                                                          {$set: newProperties},
+                                                          function(err, res) {
+                                                          if (err) throw err;
+                                                          console.log("Update Compelete.")
+                                                          })
+}
+
+
+             
+
+
+
+/*
+ Given CSA's JID, Increment no of sessions
+ --------------------------------
+ IncrementAgentSession("testJID")
+ */
+
+
+async function IncrementAgentSession(jid) {
+    // returns a document that supports JSON format.
+    let JSONObj = await client.db(dbName).collection('Agent').findOne(
+                                                              {'jid' : jid},
+                                                              {projection : {
+                                                                             'currentActiveSessions' : 1 ,
+                                                                             'reserve' : 1
+                                                                      }})
+    if (JSONObj == null) {
+        console.log("Wrong JID input")
+        return}
+    if (JSONObj.currentActiveSessions < JSONObj.reserve ) {
+        // increment by currentActiveSessions by 1
+        let newActiveSession = JSONObj.currentActiveSessions +=1
+        console.log(newActiveSession)
+        
+        
+        await client.db(dbName).collection('Agent').updateOne(
+                                                              {'jid' : jid},
+                                                              {$set: {'currentActiveSessions' : newActiveSession}},
+                                                              function(err, res) {
+                                                              if (err) throw err;
+                                                                console.log("Number of Session has been incremented.")
+                                                              })
+    }
+    else {
+        console.log("Please wait. Current CSA is working at maximum capacity")
+    }
+    
+}
+
+/**
+ Create a function that listens for engage/disengage
+ --------------------------------
+ toggleAvail("TestJID")
+ */
+
+async function toggleAvail(jid) {
+    // First check the availability of the CSA
+    let JSONObj = await client.db(dbName).collection('Agent').findOne(
+                                                                      {'jid' : jid},
+                                                                      {projection : {'availability' : 1 }})
+    if (JSONObj == null) {
+        console.log("Wrong JID input")
+        return
+    }
+    console.log(JSONObj.availability)
+    if (JSONObj.availability == false) {
+        console.log("CSA agent is still unavailable. Please wait a while more")
+    }
+    await client.db(dbName).collection('Agent').updateOne(
+                                                          {'jid' : jid},
+                                                          {$set: {'availability' : false}},
+                                                          function(err, res) {
+                                                          if (err) throw err;
+                                                          console.log("CSA is now available")
+                                                          })
+}
+    
 
 
 
 
-const updateavail = function(db, callback) {
-                  // Get the documents collection
-                  const collection = db.collection('Agents');
-                  // Update document where a is 2, set b equal to 1
-                  collection.updateOne() ;
-};
 
 
 
 
 module.exports = {
     checkAvail: checkAvail,
-
+    addAgent: addAgent,
+    modifyAgentProp: modifyAgentProp,
+    IncrementAgentSession : IncrementAgentSession,
+    toggleAvail : toggleAvail
 };
